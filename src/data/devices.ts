def6a1423bc6e51f47ffb3a0d1dc852de167914f
yRlwAaa <yrlwa666@gamil.com>
2026-05-02 22:14:54 +0800

    更新壁纸配置

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -43,7 +43,7 @@ export const devicesData: DeviceCategory = {
 		},
 		{
 			name:"Desktop PC",
-			image:"images/device/pc.png",
+			image:"/images/device/pc.png",
 			specs:"E52680v4/Samsung DDR4 8GB*2/Greatwall 128GB SSD,WD 4TB HDD/RX580 4GB/X99-C610/航嘉600w",
 			description:" 第一台亲手diy的神机！高中以来从e31220v2+8GB内存+gtx650到现在的主机，他是我正式迈入信息世界的大门。",
 		},
@@ -51,7 +51,7 @@ export const devicesData: DeviceCategory = {
 	手环:[
 		{
 			name:"小米手环9 NFC 银色",
-			image:"images/device/xm.png",
+			image:"/images/device/xm.png",
 			specs:"NFC/1.69英寸AMOLED彩屏/电池容量233Mah/蓝牙5.4/重量15.8g/110种运动模式",
 			description:"人生中第一个智能可穿戴设备，终于知道自己睡多久了。刷地铁太方便啦！！",
 			link:"https://www.mi.com/prod/xiaomi-shouhuan-9-nfc/specs"
@@ -60,7 +60,7 @@ export const devicesData: DeviceCategory = {
 	平板:[
 		{
 			name:"iPad Pro 10.5 2017",
-			image:"images/device/ipad.png",
+			image:"/images/device/ipad.png",
 			specs:"银色/A10X/4GB/64GB/2224 x 1668 分辨率,264 ppi/ProMotion 自适应刷新率技术 120Hz/广色域显示 (P3)/Apple Pencil 1代/1200 万像素摄像头/ƒ/1.8 光圈/4K 视频拍摄 (30 fps)",
 			description:"第一台平板，购买于高三下，成功体验到了无纸化学习的方便，虽然没想象中那么神，但是对我来说也是必不可少的学习工具了。",
 			link:"https://support.apple.com/zh-cn/111927/"
