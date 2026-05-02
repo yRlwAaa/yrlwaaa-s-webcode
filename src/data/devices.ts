34e4ea2d5eb72b9e73d945f5abe785a7e184fb9d
yRlwAaa <yrlwa666@gamil.com>
2026-05-02 21:33:37 +0800

    更新壁纸配置

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -39,7 +39,7 @@ export const devicesData: DeviceCategory = {
 			specs: "M1/8GB/256GB/touch id",
 			description:
 				"代码剪辑机器，图书馆必备。"
-			link:"https://support.apple.com/zh-cn/111883"
+			link:"https://support.apple.com/zh-cn/111883/",
 		}
 	],
 };
