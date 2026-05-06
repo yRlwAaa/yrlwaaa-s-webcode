4e9d81e3ce3b6f58007beedb253acde6b1f40968
yRlwAaa <yrlwa666@gamil.com>
2026-05-06 13:02:38 +0800

    更新壁纸配置

--- src/config.ts
+++ src/config.ts
@@ -484,7 +484,7 @@ export const musicPlayerConfig: MusicPlayerConfig = {
 	floatingEntryMode: "fab", // 悬浮入口模式："default" 为独立悬浮播放器，"fab" 为集成到通用 FAB 组
 	mode: "meting", // 音乐播放器模式，可选 "local" 或 "meting"
 	meting_api:
-		"https://www.bilibili.uno/api?server=:server&type=:type&id=:id&auth=:auth&r=:r", // Meting API 地址
+		"https://metingapi.yrlwa666.workers.dev/", // Meting API 地址
 	id: "1823916938", // 歌单ID
 	server: "netease", // 音乐源服务器。有的meting的api源支持更多平台,一般来说,netease=网易云音乐, tencent=QQ音乐, kugou=酷狗音乐, xiami=虾米音乐, baidu=百度音乐
 	type: "playlist", // 播单类型
