17432e9094cd6b684635f5dc984620953f12cdd3
yRlwAaa <yrlwa666@gamil.com>
2026-05-07 18:03:38 +0800

    更新壁纸配置

--- src/config.ts
+++ src/config.ts
@@ -154,7 +154,7 @@ export const siteConfig: SiteConfig = {
 
 		// PicFlow API支持(智能图片API)
 		imageApi: {
-			enable: true, // 启用图片API
+			enable: false, // 启用图片API
 			url: "http://domain.com/api_v2.php?format=text&count=4", // API地址，返回每行一个图片链接的文本
 		},
 		// 这里需要使用PicFlow API的Text返回类型,所以我们需要format=text参数
