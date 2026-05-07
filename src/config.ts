f0bab2f618a3abc7207d210b661840affec80744
yRlwAaa <yrlwa666@gamil.com>
2026-05-07 17:50:12 +0800

    更新壁纸配置

--- src/config.ts
+++ src/config.ts
@@ -264,8 +264,8 @@ export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
 		interval: 5, // 轮播间隔时间（秒）
 	},
 	zIndex: -1, // 层级，确保壁纸在背景层
-	opacity: 0, // 壁纸透明度
-	blur: 0, // 背景模糊程度
+	opacity: 0.8, // 壁纸透明度
+	blur: 1, // 背景模糊程度
 };
 
 export const navBarConfig: NavBarConfig = {
